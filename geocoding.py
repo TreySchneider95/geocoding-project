7b2de4a7e12db4f54646ccc4c5ee2153474e32ac
TreySchneider95 <tschneid95@gmail.com>
2021-09-16 11:27:41 -0700

    finished adding flask working fine

--- geocoding.py
+++ geocoding.py
@@ -10,9 +10,10 @@ url = "https://us1.locationiq.com/v1/search.php"
 def welcome():
     return render_template('home.html')
 
-@app.route('/locate', methods = ['get', 'post'])
+@app.route('/locate', methods = ['GET', 'POST'])
 def show_location():
     address = request.form['address']
+    print(address)
     private_token = "pk.727f7e39de9a6cafee73b56668557864"
     data = {
         'key': private_token,
@@ -22,7 +23,8 @@ def show_location():
     response = requests.get(url, params=data)
     latitude = response.json()[0]['lat']
     longitude = response.json()[0]['lon']
-    print(f"The latitude of the given address is: {latitude}")
-    print(f"The longitude of the given address is: {longitude}")
-    print("Thanks for using this script")
     return render_template('show.html', latitude = latitude, longitude = longitude)
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', debug=True, port = 8000)
