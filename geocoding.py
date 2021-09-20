692c0a7a5fa49c45051d8f700f7b406d6a4ada27
TreySchneider95 <tschneid95@gmail.com>
2021-09-20 08:04:18 -0700

    cleaned the program up by making a seperate function to handle the geo location finding and added documentation throughout for a 10/10 pylint

--- geocoding.py
+++ geocoding.py
@@ -1,42 +1,59 @@
+'''Geocoding Flask project
+This program is a flask project that allows a user to input an address
+and return to the user the latitude and longitude of that address.
+
+The script requires that requests be installed as well as flask within
+the python enviroment you are running the program in.
+'''
 import requests
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 # Base Url for geocoding
-url = "https://us1.locationiq.com/v1/search.php"
+
+def get_geo(address):
+    '''
+    Function that holds all of the functionality to find latitude
+    and longitude
+    '''
+    url = "https://us1.locationiq.com/v1/search.php"
+    private_token = "pk.727f7e39de9a6cafee73b56668557864"
+    data = {
+        'key': private_token,
+        'q': address,
+        'format': 'json'
+    }
+    response = requests.get(url, params=data)
+    latitude = response.json()[0]['lat']
+    longitude = response.json()[0]['lon']
+    geo_lst = [latitude, longitude]
+    return geo_lst
 
 @app.route('/')
 def welcome():
+    '''
+    Start page with form on it
+    '''
     return render_template('home.html')
 
 @app.route('/locate', methods = ['GET', 'POST'])
 def show_location():
-    address = request.form['address']
-    private_token = "pk.727f7e39de9a6cafee73b56668557864"
-    data = {
-        'key': private_token,
-        'q': address,
-        'format': 'json'
-    }
-    response = requests.get(url, params=data)
-    latitude = response.json()[0]['lat']
-    longitude = response.json()[0]['lon']
-    return render_template('show.html', latitude = latitude, longitude = longitude)
+    '''
+    Page that returns the latitude and longitude of the address
+    entered in
+    '''
+    lst = get_geo(request.form['address'])
+    return render_template('show.html', latitude = lst[0], longitude = lst[1])
 
 @app.route('/curl-test/<address>')
 def show_curl_location(address):
+    '''
+    Dummy route used only for curl testing with bash file
+    '''
     address = address.replace(' ', '-')
-    private_token = "pk.727f7e39de9a6cafee73b56668557864"
-    data = {
-        'key': private_token,
-        'q': address,
-        'format': 'json'
-    }
-    response = requests.get(url, params=data)
-    latitude = response.json()[0]['lat']
-    longitude = response.json()[0]['lon']
-    return render_template('curlshow.html', latitude = latitude, longitude = longitude)
+    lst = get_geo(request.form['address'])
+    return render_template('curlshow.html', latitude = lst[0], longitude = lst[1])
 
 
 if __name__ == '__main__':
